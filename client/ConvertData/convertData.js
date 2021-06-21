5bee3bf4ec235f5c73766343ea1c5deee007130b
Brian McGonagill <bmcgonag@gmail.com>
2021-06-20 19:57:18 -0500

    Extra views for data handling

---
+++ client/ConvertData/convertData.js
@@ -0,0 +1,17 @@
+import { ImportData } from '../../imports/api/importData.js';
+
+Template.convertData.onCreated(function() {
+    this.subscribe("importObject");
+});
+
+Template.convertData.onRendered(function() {
+
+});
+
+Template.convertData.helpers({
+
+});
+
+Template.convertData.events({
+
+});
