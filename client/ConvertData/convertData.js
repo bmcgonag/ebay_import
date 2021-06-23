61f20f281ae1c39763e059ace561540495dae6b1
Brian McGonagill <bmcgonag@gmail.com>
2021-06-23 12:34:25 -0500

    Adding data conversion functions

--- client/ConvertData/convertData.js
+++ client/ConvertData/convertData.js
@@ -5,13 +5,30 @@ Template.convertData.onCreated(function() {
 });
 
 Template.convertData.onRendered(function() {
-
+    Session.set("showDataNow", false);
 });
 
 Template.convertData.helpers({
-
+    showData: function() {
+        return Session.get("showDataNow");
+    },
+    dataPreview: function() {
+        let queryKey = Session.get("queryKey");
+        let convertTo = Session.get("convertTo");
+        let queryParam = {};
+        queryParam[queryKey] = 1;
+        
+        return ImportData.find({}, queryParam);
+    },
 });
 
 Template.convertData.events({
-
+    'click #showDataPreview' (event) {
+        event.preventDefault();
+        let key = $("#data-field").val();
+        let convType = $("#convert-to").val();
+        Session.set("queryKey", key);
+        Session.set("convertTo", convType);
+        Session.set("showDataNow", true);
+    },
 });
