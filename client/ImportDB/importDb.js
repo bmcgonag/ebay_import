b19212125d9c57c5b871a6d7ecf12eaa9ab28721
Brian McGonagill <bmcgonag@gmail.com>
2021-08-15 19:23:52 -0500

    Adding back the ability to export the MongoDB Data to MySQL, MariaDB, Postgres, etc.

--- client/ImportDB/importDb.js
+++ client/ImportDB/importDb.js
@@ -6,12 +6,32 @@ Template.importDb.onCreated(function() {
 
 Template.importDb.onRendered(function() {
     Session.set("dbPull", "none");
+    Session.set("dbhostval", false);
+    Session.set("dbuserval", false);
+    Session.set("dbpassval", false);
+    Session.set("dbportval", false);
+    Session.set("dbnameval", false);
 });
 
 Template.importDb.helpers({
     dbPull: function() {
         return Session.get("dbPull");
-    }
+    },
+    fieldsGood: function() {
+        let dbhost = Session.get("dbhostval");
+        let dbname = Session.get("dbnameval");
+        let dbuser = Session.get("dbuserval");
+        let dbpass = Session.get("dbpassval");
+        let dbport = Session.get("dbportval");
+
+        if (dbhost == false || dbname == false || dbuser == false || dbpass == false || dbport == false) {
+            console.log("returning disabled.");
+            return "disabled";
+        } else {
+            console.log("returning empty string.");
+            return "";
+        }
+    },
 });
 
 Template.importDb.events({
@@ -21,6 +41,7 @@ Template.importDb.events({
         let dbname = $("#dbname").val();
         let dbuser = $("#dbuser").val();
         let dbpass = $("#dbpass").val();
+        let dbport = $("#dbport").val();
 
         Meteor.call('pullDataFromMariaDB', dbhost, dbuser, dbpass, dbname, function(err, result) {
             if (err) {
@@ -31,5 +52,108 @@ Template.importDb.events({
                 Session.set("dbPull", "success");
             }
         });
+    },
+    'click #exportToDb' (event) {
+        event.preventDefault();
+
+        let dbhost = $("#dbhost").val();
+        let dbname = $("#dbname").val();
+        let dbuser = $("#dbuser").val();
+        let dbpass = $("#dbpass").val();
+        let dbport = $("#dbport").val();
+
+        // call the method to export the data to Maria DB.
+
+    },
+    'click #saveConnectionInfo' (event) {
+        event.preventDefault();
+
+        let dbhost = $("#dbhost").val();
+        let dbname = $("#dbname").val();
+        let dbuser = $("#dbuser").val();
+        let dbpass = $("#dbpass").val();
+        let dbport = $("#dbport").val();
+
+        if (dbhost == "" || dbhost == null) {
+            console.log("dbhost is required.");
+            return;
+        }
+
+        if (dbname == "" || dbname == null) {
+            console.log("dbname is required.");
+            return;
+        }
+
+        if (dbuser == "" || dbuser == null) {
+            console.log("dbuser is required.");
+            return;
+        }
+
+        if (dbpass == "" || dbpass == null) {
+            console.log("dbpass is required.");
+            return;
+        }
+
+        if (dbport == "" || dbport == null) {
+            console.log("dbport is required.");
+            return;
+        }
+
+        // call method to save the data to the database now.
+
+
+    },
+    'focusout #dbhost' (event) {
+        let dbhost = $("#dbhost").val();
+        if (dbhost == "" || dbhost == null) {
+            Session.set("dbhostval", false);
+        } else {
+            Session.set("dbhostval", true);
+        }
+    },
+    'focusout #dbuser' (event) {
+        let dbuser = $("#dbuser").val();
+        if (dbuser == "" || dbuser == null) {
+            Session.set("dbuserval", false);
+        } else {
+            Session.set("dbuserval", true);
+        }
+    },
+    'focusout #dbname' (event) {
+        let dbname = $("#dbname").val();
+        if (dbname == "" || dbname == null) {
+            Session.set("dbnameval", false);
+        } else {
+            Session.set("dbnameval", true);
+        }
+    },
+    'focusout #dbpass' (event) {
+        let dbpass = $("#dbpass").val();
+        if (dbpass == "" || dbpass == null) {
+            Session.set("dbpassval", false);
+        } else {
+            Session.set("dbpassval", true);
+        }
+    },
+    'focusout #dbport' (event) {
+        let dbport = $("#dbport").val();
+        if (dbport == "" || dbport == null) {
+            Session.set("dbportval", false);
+        } else {
+            Session.set("dbportval", true);
+        }
+    },
+    'click #testConnection' (event) {
+        event.preventDefault();
+
+        let dbhost = $("#dbhost").val();
+        let dbname = $("#dbname").val();
+        let dbuser = $("#dbuser").val();
+        let dbpass = $("#dbpass").val();
+        let dbport = $("#dbport").val();
+
+        // Call the method to test the db connection
+
+        
     },
 });
--- imports/api/importData.js
+++ imports/api/importData.js
@@ -471,4 +471,33 @@ Meteor.methods({
     'removeTheData' () {
         return ImportData.remove({});
     },
+    'saveDataToMariaDB' () {
+        let dataToImport = ImportData.find({}).fetch();
+
+        let numberFiles = dataToImport.length;
+
+        // connect to mariadb
+        const connection = mysql.createConnection({
+            host: '192.168.7.125',
+            user: 'brian',
+            password: 'R3tr0gr4de!',
+            database: 'ebay_data'
+        });
+
+
+
+        for (i = 0; i < numberFiles; i++) {
+            connection.query(
+                "UPDATE ebay_info SET `Sales Record Number` = " + dataToImport[i].SalesRecordNumber + ",`Order Number` = " + dataToImport[i].OrderNumber + ",`Buyer Username` = " + dataToImport[i].BuyerUsername + ",`Buyer Name` = " + dataToImport[i].BuyerName + ",`Buyer Note` = " + dataToImport[i].BuyerNote + ",`Buyer Address 1` = " + dataToImport[i].BuyerAddress1 + ",`Buyer Address 2` = " + dataToImport[i].BuyerAddress2 + ",`Buyer City` = " + dataToImport[i].BuyerCity + ",`Buyer State` = " + dataToImport[i].BuyerState + ",`Buyer Zip` = " + dataToImport[i].BuyerZip + ",`Buyer Country` = " + dataToImport[i].BuyerCountry + ",`Ship To Name` =  " + dataToImport[i].ShipToName + ",`Ship To Phone` = " + dataToImport[i].ShipToPhone + ",`Ship To Address 1` = " + dataToImport[i].ShipToAddress1 + ",`Ship To Address 2` = " + dataToImport[i].ShipToAddress2 + ",`Ship To City` = " + dataToImport[i].ShipToCity + ",`Ship To State` = " + dataToImport[i].ShipToState + ",`Ship To Zip` = " + dataToImport[i].ShipToZip + ",`Ship To Country` = " + dataToImport[i].ShipToCounty + ",`Item Number` = " + dataToImport[i].ItemNumber + ",`Item Title` = " + dataToImport[i].ItemTitle+ ",`Size` = " + dataToImport[i].Size + ",`Custom Label` = " + dataToImport[i].CustomLabel + ",`Sold Via Promoted Listings` = " + dataToImport[i].SoldViaPromotedListings + ",`Quantity` = " + dataToImport[i].Quantity + ",`Sold For` =  " + dataToImport[i].SoldFor + ",`Shipping and Handling` = " + dataToImport[i].ShippingAndHandling + ",`Seller Collected Tax` = " + dataToImport[i].SellerCollectedTax + ",`eBay Collected Tax` = " + dataToImport[i].eBayCollectedTax + ",`Total Price` = " + dataToImport[i].TotalPrice + ",`Payment Method` = " + dataToImport[i].PaymentMethod + ",`Sale Date` = " + dataToImport[i].SaleDate + ",`Year` = " + dataToImport[i].Year + ",`SaleDayName` = " + dataToImport[i].SaleDayName + ",`SaleMonthName` = " + dataToImport[i].SaleMonthName + ",`Paid On Date` = " + dataToImport[i].PaidOnDate + ",`Ship By Date` = " + dataToImport[i].ShipByDate + ",`Minimum Estimated Delivery Date` = " + dataToImport[i].MinimumEstimatedDeliveryData + ",`Maximum Estimated Delivery Date` = " + dataToImport[i].MaximumEstimatedDeliveryDate + ",`Shipped On Date` = " + dataToImport[i].ShippedOnDate + ",`Feedback Left` = " + dataToImport[i].FeedbackLeft + ",`Feedback Received` = " + dataToImport[i].FeedbackReceived + ",`My Item Note` = " + dataToImport[i].MyItemNote + ",`PayPal Transaction ID` = " + dataToImport[i].PayPalTransactionID + ",`Shipping Service` = " + dataToImport[i].ShippingService + ",`Tracking Number` = " + dataToImport[i].TrackingNumber + ",`Transaction ID` = " + dataToImport[i].TransactionID + ",`Variation Details` = " + dataToImport[i].VariationDetails + ",`Global Shipping Program` = " + dataToImport[i].GlobalShippingProgram + ",`Global Shipping Reference ID` = " + dataToImport[i].GlobalShippingReferenceID + ",`Click And Collect` = " + dataToImport[i].ClickAndCollect + ",`Click And Collect Reference Number` = " + dataToImport[i].ClickAndCollectReferenceNumber + ",`eBay Plus` = " + dataToImport[i].eBayPlus + ",`eBay Collected Tax And Fees Included in Total` = " + dataToImport[i].eBayCollectedTaxAndFeesInTotal,
+                    function(err, results, fields) {
+                        if (err) {
+                            console.log("Error uploading to mariadb: " + err);
+                        } else {
+                            console.log(results);
+                            console.log(fields);
+                        } 
+                    }
+            );
+        }
+    },
 });
